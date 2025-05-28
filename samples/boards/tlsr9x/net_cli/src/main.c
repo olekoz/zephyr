51dbd6ab13aed9ecb87119c4df1619dd9e677674
Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>
2025-05-28 17:59:36 +0300

    WIP: ICMP reception.

--- samples/boards/tlsr9x/net_cli/src/main.c
+++ samples/boards/tlsr9x/net_cli/src/main.c
@@ -9,8 +9,46 @@
 #include <zephyr/net/openthread.h>
 #include <openthread/border_routing.h>
 
+#include <zephyr/net/icmp.h>
+
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+static struct net_icmp_ctx icmp_rs_ctx, icmp_ra_ctx;
+
+static int icmp_input(struct net_icmp_ctx *ctx, struct net_pkt *pkt,
+	struct net_icmp_ip_hdr *hdr, struct net_icmp_hdr *icmp_hdr, void *user_data)
+{
+	if (hdr->family != AF_INET6) {
+		return 0;
+	}
+
+	size_t tot_len = net_pkt_get_len(pkt);
+	uint8_t ip_len = net_pkt_ip_hdr_len(pkt);
+
+	if (tot_len > ip_len) {
+		uint8_t icmp_data[tot_len - ip_len];
+		struct net_pkt_cursor bkp;
+
+		net_pkt_cursor_backup(pkt, &bkp);
+		net_pkt_cursor_init(pkt);
+		do {
+			if (net_pkt_skip(pkt, ip_len) < 0) {
+				break;
+			}
+			if (net_pkt_read(pkt, icmp_data, sizeof(icmp_data)) < 0) {
+				break;
+			}
+			struct in6_addr *src = (struct in6_addr *) hdr->ipv6->src;
+
+			/* call otPlatInfraIfRecvIcmp6Nd */
+			LOG_HEXDUMP_INF(src, sizeof(struct in6_addr), "icmp from");
+			LOG_HEXDUMP_INF(icmp_data, sizeof(icmp_data), "icmp data");
+		} while (0);
+		net_pkt_cursor_restore(pkt, &bkp);
+	}
+	return 0;
+}
 
 void wifi_changed(struct net_if *iface, bool is_connected)
 {
@@ -22,6 +60,10 @@ void wifi_changed(struct net_if *iface, bool is_connected)
 			net_if_get_by_iface(net_if_get_default()), true);
 
 		if (err == OT_ERROR_NONE) {
+			(void) net_icmp_init_ctx(&icmp_rs_ctx, 133, 0, icmp_input);
+			icmp_rs_ctx.iface = iface;
+			(void) net_icmp_init_ctx(&icmp_ra_ctx, 134, 0, icmp_input);
+			icmp_ra_ctx.iface = iface;
 			err = otBorderRoutingSetEnabled(openthread_get_default_instance(), true);
 			if (err == OT_ERROR_NONE) {
 				LOG_INF("openthread border router enabled");
@@ -34,6 +76,8 @@ void wifi_changed(struct net_if *iface, bool is_connected)
 	} else {
 		LOG_WRN("** wifi disconnected");
 		otError err = otBorderRoutingSetEnabled(openthread_get_default_instance(), false);
+		(void) net_icmp_cleanup_ctx(&icmp_rs_ctx);
+		(void) net_icmp_cleanup_ctx(&icmp_ra_ctx);
 
 		if (err == OT_ERROR_NONE) {
 			LOG_INF("openthread border router disabled");
